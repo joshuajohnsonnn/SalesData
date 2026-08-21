93598921b64eab0fd870ed9b80e77119e954598e
Joshua Johnson <trillions22@hotmail.com>
2026-08-21 17:37:37 +0100

    Add temporary test variables to salesdata.py
    These lines are added for testing and debugging purposes and should be ignored for production logic. They serve as a marker for verifying local environment changes.

--- src/salesdata.py
+++ src/salesdata.py
@@ -108,3 +108,6 @@ else:
     from data.data import data
     pdata1(data)
        
+test = 1 #ignore this line, it's just for testing purposes
+test = 2 #ignore this line, it's just for testing purposes
+test = 3 #ignore this line, it's just for testing purposes
